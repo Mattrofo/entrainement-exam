d9327dfab595e8fe84017b0cc7a734137e0a3b20
Ralambotsirofo Mathieu <m.ralambotsirofo@gmail.com>
2023-03-27 17:50:55 +0200

    page view done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+
   def index
     @posts = Post.all
   end
@@ -15,6 +16,22 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(set_params)
     redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(set_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+
+p "------ Starting deleting data ----------"
+
+Restaurant.destroy_all
+
+p "------ Create data ----------"
+
+10.times do
+
+end
